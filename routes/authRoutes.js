db3ce3bde7f384242b0cd6de436239a569b2c0fc
lexy <lexyblazy@gmail.com>
2018-06-07 01:00:23 +0100

    wired up authentication logic

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,36 @@
-const router = require("express").Router({ mergeParams: true });
+const router = require("express").Router();
+const passport = require("passport");
+const { logout } = require("../controllers/authControllers");
 
-router.get("/auth/twitter", (req, res) => {
-  res.send("This will handle twitter authentication");
+router.get("/auth/twitter", passport.authenticate("twitter"));
+
+router.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter", { failureRedirect: "/api" }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "back",
+    successRedirect: ""
+  }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
+// fetch the current user
+router.get("/api/current_user", (req, res) => {
+  res.send(req.user);
 });
 
-router.get("/auth/twitter/callback", (req, res) => {
-  res.send("This will handle the callback");
-});
+router.get("/api/logout", logout);
 
 module.exports = router;
